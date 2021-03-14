22784327c44c4033363fe2cd20705f4abbbba177
tebanban <estebancalf@gmail.com>
2021-03-14 22:17:19 +0000

    initial commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,23 +1,31 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
+	const [nasaImage, setNasaImage] = useState(rigoImage);
+	const nasaLogo =
+		"https://www.nasa.gov/sites/default/files/styles/side_image/public/thumbnails/image/nasa-logo-web-rgb.png?itok=uDhKSTb1";
+	useEffect(() => {
+		let fetchUrl = "https://api.nasa.gov/planetary/apod";
+		async function fetchImage() {
+			let resultado = await fetch(fetchUrl)
+				.then(res => res.json())
+				.then(data => data.url)
+				.catch(err => console.log(err));
+			console.log(resultado);
+			setNasaImage(resultado);
+		}
+		fetchImage();
+	});
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
+			<h1>Hello from Space!</h1>
 			<p>
-				<img src={rigoImage} />
+				<img src={nasaLogo} />
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
 			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+				<img height="500px" src={nasaImage} />
 			</p>
 		</div>
 	);
